3337e92407d2e4682d269589df47cf74f83b2b0b
Jesús Quintana <jesquiliche@hotmail.comm>
2023-03-16 21:48:56 +0100

    Añadido modificaciones a Login

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,11 +1,11 @@
 import React, { useState, useContext } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import { UserContext } from "../context/UserProvider";
 import Cookies from "universal-cookie";
 
 const Login = () => {
   const navigate = useNavigate();
-  const { user, signIn } = useContext(UserContext);
+  const {signIn } = useContext(UserContext);
   const [loading, setLoading] = useState(false);
 
   const [datos, setDatos] = useState({
@@ -22,69 +22,43 @@ const Login = () => {
     console.log(datos);
   };
 
-  const handleOnsubmit = (e) => {
+  const handleOnsubmit = async (e) => {
     e.preventDefault();
     setLoading(true);
-    
-    ObtenerToken(datos);
-    console.log(loading)
+    await ObtenerToken(datos);
     setLoading(false);
   };
 
-  let token;
   const cookies = new Cookies();
 
-  /* token = cookies.get("token");
-  if(token!==undefined) {
-    setDataErr(null);
-    signIn();
-    
-    return navigate("/Tablero");
-
-  }*/
-
-  //Insertar en base de datos
   const ObtenerToken = async (datos) => {
     try {
-      const cookies = new Cookies();
+      cookies.remove("token");
 
-      token = cookies.get("token");
-      if (token === undefined) {
-        //Llamamos al EndPoint que nos devuelve el token
-        const data = await fetch("http://localhost:8000/api/login", {
-          method: "POST",
-          body: JSON.stringify(datos), // data can be `string` or {object}!
+      const response = await fetch("http://localhost:8000/api/login", {
+        method: "POST",
+        body: JSON.stringify(datos),
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+        },
+      });
+      const data = await response.json();
 
-          headers: {
-            "Content-Type": "application/json",
-            "Access-Control-Allow-Origin": "*",
-          },
-        })
-          .then((response) => response.json())
-          .then((data) => {
-            //Guardamos en una cookie
-            cookies.set("token", data.authorisation.token, {
-              path: "/",
-              maxAge: 60 * 60 * 24,
-            });
-            setDataErr(null);
-            signIn();
-            navigate("/Tablero");
-          })
-          //Si se produce un error los datos inicio de sesión son incorrectos
-          // o el servidor no esta disponible
-          .catch((error) => {
-            setDataErr("Credenciales no validas");
-          });
-        //Si todo fue correcto enviar a página de inicio
-
-      } else {
-        setDataErr(null);
-        signIn();
-        navigate("/Tablero");
+      if (!response.ok) {
+        throw new Error("Credenciales no válidas");
       }
+
+      cookies.set("token", data.authorisation.token, {
+        path: "/",
+        maxAge: 60 * 60 * 24,
+      });
+
+      setDataErr(null);
+      signIn();
+      navigate("/Tablero");
     } catch (error) {
-      console.log(error);
+      setDataErr(error.message);
     }
   };
 
@@ -114,8 +88,6 @@ const Login = () => {
                 onChange={handleOnChange}
                 required
               />
-
-
               {loading ? (
                 <div className="text-center my-2">
                   <div className="spinner-border text-primary" role="status">
@@ -136,7 +108,10 @@ const Login = () => {
               ""
             )}
             <p className="text-dark">
-              ¿No estas registrado?<a className="text-primary"> Registrate</a>
+              ¿No estas registrado?
+              <Link to="/registrar">
+                <span className="text-primary"> Registrate</span>
+              </Link>
             </p>
           </div>
         </div>
